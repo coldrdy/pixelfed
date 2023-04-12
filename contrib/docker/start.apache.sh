c8a311b68cea9ec2cef0cd9513a5ebc0a98fdf6e
umarhadi <hi@umarhadi.dev>
2023-04-12 17:19:09 +0800

    add fly conf

--- contrib/docker/start.apache.sh
+++ contrib/docker/start.apache.sh
@@ -6,10 +6,16 @@ chown -R www-data:www-data storage/ bootstrap/
 
 # Refresh the environment
 php artisan storage:link
+php artisan horizon:install
 php artisan horizon:publish
+php artisan route:clear
 php artisan route:cache
+php artisan view:clear
 php artisan view:cache
+#php artisan config:clear
 php artisan config:cache
+php artisan migrate --force
 
-# Finally run Apache
+# apache2-foreground & php artisan horizon
+
 apache2-foreground
